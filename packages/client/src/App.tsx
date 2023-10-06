e7c863675dbd81740896d471e74e3d6a88ebd74d
Okan <huseyinokand@gmail.com>
2023-10-06 18:46:13 +0300

    first commit

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,38 @@
+import { useComponentValue } from "@latticexyz/react";
+import { useMUD } from "./MUDContext";
+import { singletonEntity } from "@latticexyz/store-sync/recs";
+
+export const App = () => {
+  const {
+    components: { Counter },
+    systemCalls: { increment, decrement },
+  } = useMUD();
+
+  const counter = useComponentValue(Counter, singletonEntity);
+
+  return (
+    <>
+      <div>
+        Counter: <span>{counter?.value ?? "??"}</span>
+      </div>
+      <button
+        type="button"
+        onClick={async (event) => {
+          event.preventDefault();
+          console.log("new counter value:", await increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        type="button"
+        onClick={async (event) => {
+          event.preventDefault();
+          console.log("new counter value:", await decrement());
+        }}
+      >
+        Decrement
+      </button>
+    </>
+  );
+};
